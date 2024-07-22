03e8649a08bff1a4af0b5374dcdffbe44c174ef9
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-22 17:43:39 +0800

    fix header

--- resources/views/includes/admin/header.blade.php
+++ resources/views/includes/admin/header.blade.php
@@ -16,16 +16,16 @@
                     <h5>{{ Auth::user()->nama }}</h5>
                 </li>
                 <li class="nav-item dropdown">
-                    {{-- <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
+                    <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="{{ asset('assets/images/profile/user-1.jpg') }}" alt="" width="35"
                             height="35" class="rounded-circle">
-                    </a> --}}
+                    </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
                         <div class="message-body">
                             <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
                                 <i class="ti ti-user fs-6"></i>
-                                <p class="mb-0 fs-3">My Profile</p>
+                                <p class="mb-0 fs-3">Profile</p>
                             </a>
                             <form action="{{ route('user-logout') }}" method="POST">
                                 @csrf
